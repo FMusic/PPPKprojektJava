b527bf42aacfc8ff80c17bd89bb52577c7078778
Frane Mušić <frane.music@gmail.com>
2020-02-19 01:36:54 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import com.fm.ui.DriversForm;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        new DriversForm();
+    }
+}
---
+++ src/com/fm/ui/DriversForm.java
@@ -0,0 +1,16 @@
+package com.fm.ui;
+
+import javax.swing.*;
+
+public class DriversForm extends JFrame{
+    private JPanel pnlMain;
+    private JButton btnOpenFile;
+    private JButton btnSave;
+    private JLabel lblStatus;
+
+    public DriversForm(){
+        setContentPane(pnlMain);
+        setSize(350, 100);
+        setVisible(true);
+    }
+}
